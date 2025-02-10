9aaaaaeffae9a19361422b2e773164825362a3df
mia16 <mahnur.imran@gmail.com>
2025-02-10 00:58:21 -0800

    added buttons for book finder js

---
+++ root/js/bookfinder.js
@@ -0,0 +1,6 @@
+const sendBtn = document.getElementById('#send-btn');
+const userInput = document.getElementById('#user-input');
+const botMessage = document.querySelector('.bot-message');
+const chatWindow = document.getElementById('#chat-window');
+
+
